c9d42680564e3a1cac0dffdfa6b22cfefe20c4a7
Meg Vandezande <meg.vandezande@gmail.com>
2022-06-07 16:42:08 +0200

    Set current user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = session['user'] ? User.find(session['user']['id']) : User.first
+    #comment: default current_user set for testing purposes. This is not something to do in prod of course.
+  end
 end
